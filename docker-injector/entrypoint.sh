64f6e247dd62462d8356303d8dcdd357a9993889
unknown <sellerin@users.noreply.github.com>
2019-05-11 13:49:28 +0200

    refactor project organization

---
+++ docker-injector/entrypoint.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+#Launch test
+$GATLING_HOME/bin/gatling.sh -s $SIMULATION_NAME
---
+++ go-gcluster-cli/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+    gclient "github.com/sellerin/kubernetes-go-client"
+)
+
+func main() {
+    gclient.test()
+}
