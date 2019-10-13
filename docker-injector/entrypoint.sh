0bc8b1902b5481678a338a5fa25cccac7260da88
alain <alain@localhost.localdomain>
2019-10-13 11:07:00 +0200

    handle data env variable

--- docker-injector/entrypoint.sh
+++ docker-injector/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export JAVA_OPTS="-Dusers=$NBUSERS -Dramp=$RAMP -Dduration=$DURATION"
+export JAVA_OPTS="-Dusers=$NBUSERS -Dramp=$RAMP -Dduration=$DURATION -Ddata=$DATA"
 
 #Launch test
 $GATLING_HOME/bin/gatling.sh -s $SIMULATION_NAME
